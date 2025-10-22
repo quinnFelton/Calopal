069d1b3a0178d6c822bd10a40365f80f0d660bb1
Vinh An Nguyen <166468851+VNGucsc@users.noreply.github.com>
2025-10-21 17:00:38 -0700

    Moving home and goal screen to different files.

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -1,40 +1,19 @@
-import { Text, View } from "react-native";
 //import { useState } from "react";
-import foodDriver from "../screens/foodDriver";
 //import { styles } from "./styles";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import foodDriver from "../screens/foodDriver";
+import goalScreen from "../screens/goalScreen";
+import HomeScreen from "../screens/homeScreen";
 
 const Tab = createBottomTabNavigator();
 
-function HomeScreen() {
-  return (
-    <View>
-      <Text>"This is the home screen.</Text>
-    </View>
-  )
-}
 
-function GoalScreen() {
-  return (
-    <View>
-      <Text>"This is the goal screen.</Text>
-    </View>
-  )
-}
-
-function FoodInputScreen() {
-  return (
-    <View>
-      <Text>"This is the food input screen.</Text>
-    </View>
-  )
-}
 
 export default function Index() {
     return (
           <Tab.Navigator>
             <Tab.Screen name="Home" component = {HomeScreen}/>
-            <Tab.Screen name="Goals" component = {GoalScreen}/>
+            <Tab.Screen name="Goals" component = {goalScreen}/>
             <Tab.Screen name="Input" component = {foodDriver}/>
           </Tab.Navigator>
       );
---
+++ app/screens/goalScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+//Currently just placeholder information to verify that nav bar works. | 10/21/2025 | Vinh
+
+export default function goalScreen() {
+  return (
+    <View>
+      <Text>This is the home screen.</Text>
+    </View>
+  )
+}
