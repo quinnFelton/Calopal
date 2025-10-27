c6aa3bd47d0090790520d4f1f8b7824a7383298e
Vinh An Nguyen <166468851+VNGucsc@users.noreply.github.com>
2025-10-27 13:30:57 -0700

    Changing out icons for Nav Bar to 16 bit icons.

--- app/src/navigation/index.tsx
+++ app/src/navigation/index.tsx
@@ -1,27 +1,20 @@
 //import { useState } from "react";
 //import { styles } from "./styles";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Ionicons from '@expo/vector-icons/Ionicons';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from 'react-native';
 import FoodDriver from "../screens/foodDriver";
 import GoalScreen from "../screens/goalScreen";
 import HomeScreen from "../screens/homeScreen";
 
 const Tab = createBottomTabNavigator();
 
+//Might eventually change to a custom tarBar component instead to allow for more flexibility with button placements and such.
+//This works for now as its functional and looks decent enough.
+
 export default function Index() {
     return (
           <Tab.Navigator
             screenOptions={({ route }) => ({
-              tabBarIcon: ({ color}) => {
-                if (route.name === 'Home') {
-                  return <FontAwesome name={'home'} size={35} color={color} />;
-                } else if (route.name === 'Goals') {
-                  return <FontAwesome name={'flag-checkered'} size={30} height={25} color={color} />;
-                } else {
-                  return <Ionicons name={'fast-food'} size={33} color={color} />;
-                }
-              },
               tabBarActiveTintColor: 'yellow',
               tabBarInactiveTintColor: 'black',
               tabBarActiveBackgroundColor: '#CDB500',
@@ -32,9 +25,39 @@ export default function Index() {
             })}
             backBehavior= 'firstRoute'
           >
-            <Tab.Screen name="Home" component = {HomeScreen}/>
-            <Tab.Screen name="Goals" component = {GoalScreen}/>
-            <Tab.Screen name="Input" component = {FoodDriver}/>
+            <Tab.Screen 
+              name="Home" 
+              component = {HomeScreen}
+              options={{
+                tabBarIcon: ({size,focused,color}) => {
+                  return (
+                    <Image source={require('./Nav Bar Icons/home_icon.png')}
+                    style={{width: 30, height: 30, tintColor: focused ? 'yellow' : 'black'}}/>
+                  );
+                },
+              }}/>
+            <Tab.Screen 
+              name="Goals" 
+              component = {GoalScreen}
+              options={{
+                tabBarIcon: ({size,focused,color}) => {
+                  return (
+                    <Image source={require('./Nav Bar Icons/checkered_flag.png')}
+                    style={{width: 50, height: 30, tintColor: focused ? 'yellow' : 'black'}}/>
+                  );
+                },
+              }}/>
+            <Tab.Screen 
+              name="Input" 
+              component = {FoodDriver}
+              options={{
+                tabBarIcon: ({size,focused,color}) => {
+                  return (
+                    <Image source={require('./Nav Bar Icons/food_icon.png')}
+                    style={{width: 35, height: 35, tintColor: focused ? 'yellow' : 'black'}}/>
+                  );
+                },
+              }}/>
           </Tab.Navigator>
     );
 }
